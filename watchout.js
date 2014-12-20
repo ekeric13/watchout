8711efa2115487064f969d89689765291a0cf3ba
Eric Kennedy <ekeric13@gmail.com>
2014-12-20 14:20:02 -0800

    added music, player collision detection, powerup, and more enemies

--- watchout.js
+++ watchout.js
@@ -1,8 +1,12 @@
 // start slingin' some d3 here.
 var scoreCount = 0;
+var highScoreCount = 0;
 var collisionCount = 0;
 var timerOn = false;
-
+var radius = 1;
+var playerText = ""
+var audio = d3.select("#gameMusic")
+// audio.style("display", "none")
 // console.log(scoreCount)
 
 // build board
@@ -18,17 +22,23 @@ var board = d3.select(".board")
 // make player move
 // build enemies
 
-var Enemy = function(numEnemies){
+
+
+var Enemy = function(numEnemies1, numEnemies2){
+  console.log(this);
   this.datum = [];
-  this.numEnemies = numEnemies
+  this.datum2 = [];
+  this.numEnemies1 = numEnemies1;
+  this.numEnemies2 = numEnemies2;
+  this.shield = 1;
 
 }
 
 Enemy.prototype.createEnemy = function(){
 
-  for (var i = 0; i < this.numEnemies; i++){
-    var x = Math.random() * 500;
-    var y = Math.random() * 500;
+  for (var i = 0; i < this.numEnemies1; i++){
+    var x = Math.random() * 600;
+    var y = Math.random() * 600;
     this.datum[i] = {"x": x, "y":  y };
   };
 
@@ -41,10 +51,51 @@ Enemy.prototype.createEnemy = function(){
                     .attr("cy", function(d){ return d.y})//Math.random() * 500)
                     .attr("r", "15px")
                     .attr("fill", "red")
-                    .on({"mousemove": playerPosition, "mouseover": increaseCollionCount})
+                    .on({"mouseover": increaseCollionCount})
+
+  for (var j = 0; j < this.numEnemies2; j++){
+    var x = Math.random() * 500;
+    var y = Math.random() * 500;
+    this.datum2[j] = {"x": x, "y":  y };
+  };
+
+  var enemy2 = board.selectAll(".enemy2")
+                    .data(this.datum2)
+                    .enter()
+                    .append("circle")
+                    .attr("class", function(d, i){return 'enemy2'+ i})
+                    .attr("cx", function(d){ return d.x})//Math.random() * 500)
+                    .attr("cy", function(d){ return d.y})//Math.random() * 500)
+                    .attr("r", "15px")
+                    .attr("fill", "black")
+                    .on({"mouseover": increaseCollionCount})
 
 
-//
+    var shield = board.selectAll(".shield")
+                      .data([{"x": Math.random() * 500, "y" : Math.random() * 500, "value" : "shield"}])
+                      .enter();
+
+    shield.append("circle")
+      .attr("class", "shield")
+      .attr("cx", function(d){return d.x} )
+      .attr("cy", function(d){return d.y} )
+      .attr("r", "15px")
+      .attr("fill", "green")
+      .on({"click" : powerUp});
+
+
+   shield.append("text")
+      .attr("class", "shield-text")
+     .attr("x", function(d){ return d.x})
+     .attr("y", function(d){ return d.y})
+     .attr("dx", "-1.25em")
+     .attr("dy", "-1.25em")
+     .text(function(d) { return d.value })
+     .style("font-size", 10)
+     .style("color", "black")
+
+
+
 }
 
 Enemy.prototype.moveEnemy = function(){
@@ -52,52 +103,73 @@ Enemy.prototype.moveEnemy = function(){
   var changePosition = d3.selectAll(".enemy")
                           .data(this.datum)
                           .transition().duration(1500)
-                          .tween('custom', tweenWithCollisionDetection)
-                          .attr("cx", function(d){return Math.random() * 500})
-                          .attr("cy", function(d){return Math.random() * 500})
+                          .attr("cx", function(d){return Math.random() * 600})
+                          .attr("cy", function(d){return Math.random() * 600});
+
+  var changePosition2 = d3.select(".enemy20")
+                          .data(this.datum2)
+                          .transition().duration(500)
+                          .attr("cx", function(d){return d3.select(".player").attr("cx")})
+                          .attr("cy", function(d){return d3.select(".player").attr("cy")})
+
+
+
+// give each enemy2 different transition
+}
+
+Enemy.prototype.moveEnemy2 = function(){
+
+  var changePosition3 = d3.select(".enemy21")
+                          .data(this.datum2)
+                          .transition().duration(1000)
+                          .attr("cx", function(d){return d3.select(".player").attr("cx")})
+                          .attr("cy", function(d){return d3.select(".player").attr("cy")})
+
+  var changePosition3 = d3.select(".enemy22")
+                          .data(this.datum2)
+                          .transition().duration(1250)
+                          .attr("r", "40px")
+                          .attr("cx", function(d){return d3.select(".player").attr("cx")})
+                          .attr("cy", function(d){return d3.select(".player").attr("cy")})
 
 }
 
 
 
-function playerPosition(){
-  var xMouse = d3.mouse(this)[0];
-  var yMouse = d3.mouse(this)[1];
-
-
-
-  // d3.select(".scoreboard")
-  // for (var i = 0; i < newEnemies.datum.length; i++){
-  //   if (newEnemies.datum[i].x === d3.select(".player").attr("cx") && newEnemies.datum[i].y === d3.event.y){
-  //     scoreCount++;
-  //   }
-  // }
-  // debugger;
-  var currentScore = d3.select(".current").select("span").text(scoreCount);//select("span").attr("span", 5);
-
-  // d3.event.x
-  // d3.event.y
-
-  // console.log(xMouse, yMouse);
-  // console.log(d3.event)
-}
 
 function scoreIncrease(){
   d3.select(".player").remove();
+  d3.select(".player-text").remove();
+
   var xMouse = d3.mouse(this)[0];
   var yMouse = d3.mouse(this)[1];
   // console.log(xMouse, yMouse)
 
-  board.append("circle")
+
+  var player = board.append("circle")
         .attr("class", "player")
         .attr("cx", xMouse)
         .attr("cy", yMouse - 7)
-        .attr("r", "1px")
         .attr("fill", "blue")
-  scoreCount++;
-  var currentScore = d3.select(".current").select("span").text(scoreCount);//select("span").attr("span", 5);
+        .attr("r", radius)
 
+var text = board.append("text")
+      .attr("class", "player-text")
+      .attr("x", xMouse)
+      .attr("y", yMouse)
+      .attr("dx", "-1em")
+      .attr("dy", "-1.25em")
+      .style("font-size", 10)
+      .style("color", "black")
+      .text(playerText)
+
+
+  scoreCount++;
+  highScoreCount = highScoreCount > scoreCount ? highScoreCount : scoreCount
+  var currentScore = d3.select(".current").select("span").text(scoreCount);//select("span").attr("span", 5);
+  var highScore = d3.select(".high").select("span").text(highScoreCount);
   window.clearInterval(clearScoreTimer)
+  initiateClearScore();
 
 }
 
@@ -108,59 +180,100 @@ function increaseCollionCount(){
   //debugger
   var xMouse = d3.mouse(this)[0];
 
+
+  var shield = board.selectAll(".shield")
+                      .data([{"x": Math.random() * 500, "y" : Math.random() * 500, "value" : "shield"}])
+                      .enter();
+
+    shield.append("circle")
+      .attr("class", "shield")
+      .attr("cx", function(d){return d.x} )
+      .attr("cy", function(d){return d.y} )
+      .attr("r", "15px")
+      .attr("fill", "green")
+      .on({"click" : powerUp});
+
+
+   shield.append("text")
+      .attr("class", "shield-text")
+     .attr("x", function(d){ return d.x})
+     .attr("y", function(d){ return d.y})
+     .attr("dx", "-1.25em")
+     .attr("dy", "-1.25em")
+     .text(function(d) { return d.value })
+     .style("font-size", 10)
+     .style("color", "black")
+
 }
 
-// function checkCollision(){
-//   var xMouse = d3.mouse(this)[0];
-//   var yMouse = d3.mouse(this)[1];
-//   // console.log(xMouse)
-//   // debugger;
+function powerUp() {
+  radius = 40;
+  setPlayerText();
+  setTimeout(setRadius, 5000);
+  d3.select(".shield").remove()
+  d3.select(".shield-text").remove()
+  document.getElementById("gameMusic").pause()
+  // d3.select(".power-music")
+  //     .append("embed id="gameMusic" src="gameMusic.mp3" loop="true" autostart="false">")
+
+
+}
+var clearPlayerText;
+var setPlayerText = function(){
+  playerText = 5;
+  clearPlayerText = setInterval(function(){playerText--}, 1000)
+}
+
+var setRadius = function(){
+  radius = 1;
+  window.clearInterval(clearPlayerText);
+  playerText = "";
+  // pauseVid()
+
+  // d3.select(".game-music")
+  //       .append("embed")
+  //       .attr("class", ".game-music")
+  //       .attr("src", "gameMusic.mp3")
+  //       .attr("loop", "true")
+  //       .attr("autostart", "false")
+}
+// var audio = document.getElementById("gameMusic")
+// function pauseVid() {
+//     audio.pause();
 // }
 
-var newEnemies = new Enemy(20)
+
+var newEnemies = new Enemy(20, 5)
 newEnemies.createEnemy();
 
-setInterval(console.log(newEnemies.datum[0].x), 10)
+// setInterval(console.log(newEnemies.datum[0].x), 10)
 
 
 
 // setTimeout(, 2000)
 //console.log(d3.mouse(board))
 
-setInterval(Enemy.prototype.moveEnemy.bind(newEnemies), 2000)
-// moveEnemy(20);
-var tweenWithCollisionDetection = function(){
-  var radiusSum = 30;
-  var separation;
-  var xDiff;
-  var yDiff;
-  console.log(d3.select(".player").attr("cx") )
-  d3.select(".player").attr("cx") // the x cordinate of the circle
-  d3.select(".player").attr("cy") // the y cordinate of the circle
-  d3.select(".player").attr("r") // the radius of the circle
-  for (var i = 0; i < newEnemies.datum.length; i++){
-    xDiff =  newEnemies.datum[i].x - d3.select(".player").attr("cx");
-    yDiff = newEnemies.datum[i].y - d3.select(".player").attr("cy");
-    separation = Math.sqrt( Math.pow(xDiff,2) + Math.pow(yDiff,2) );
-    if (separation < radiusSum){
-      scoreCount = 0;
-      collisionCount++;
-    }
-  }
-}
-// var clearScore = function(){
-//   setInterval(scoreCount = 0, 2000)
-// }
+setInterval(Enemy.prototype.moveEnemy.bind(newEnemies), 1500)
+setInterval(Enemy.prototype.moveEnemy2.bind(newEnemies), 500)
+
+
+
 
 var clearScore = function(){
   scoreCount = 0
-  debugger;
 }
-var clearScoreTimer = function(){
-  setInterval(clearScore, 2000)
+// var clearScoreTimer = function(){
+
+// }
+var clearScoreTimer;
+var initiateClearScore = function(){
+  clearScoreTimer = setInterval(clearScore, 1500)
 }
+initiateClearScore();
+
+
  // setTimeout(clearScore, 2000);
-setInterval(clearScoreTimer, 3000)
+// setInterval(clearScoreTimer, 3000)
 // setInterval(checkCollision, 20)
 
 
